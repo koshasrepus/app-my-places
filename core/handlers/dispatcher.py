54e0d14b7a8a341c233bb86d99ca635517917e50
Александр Евсеев <alexander.evseev@balance-pl.ru>
2021-03-21 00:56:14 +0300

    Added added stubs for handlers: start, add, list, reset, location

--- core/handlers/dispatcher.py
+++ core/handlers/dispatcher.py
@@ -5,16 +5,29 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 bot.set_webhook(url='https://acf0c0c5c755.ngrok.io/telegram_bot_path')
 
+@bot.message_handler(commands=['start'])
+def hand_start_messate(message):
+    resp = '/start - информация о боте\n/add - добавить локацию\n/list - просмотр 10 последних локаций\n' \
+           '/reset - удалить все локации\nМожно отправить локацию, чтобы просмотреть сохранённые места в радиусе 500 метров'
+    bot.send_message(chat_id=message.chat.id, text=resp)
+
 @bot.message_handler(commands=['add'])
 def hand_start_messate(message):
-    print(message.text)
-    bot.send_message(chat_id=message.chat.id, text='Hello World')
+    bot.send_message(chat_id=message.chat.id, text='command [add] is works')
 
 @bot.message_handler(commands=['list'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 ("Hi there, I am EchoBot.\n"
-                  "I am here to echo your kind words back to you."))
+def hand_list_message(message):
+    bot.send_message(chat_id=message.chat.id, text='command [list] is works')
+
+@bot.message_handler(commands=['reset'])
+def hand_reset_message(message):
+    bot.send_message(chat_id=message.chat.id, text='command [reset] is works')
+
+
+@bot.message_handler(content_types=['location'])
+def handl_location(message):
+    bot.send_message(chat_id=message.chat.id, text='command [location] is works')
+
 
 def process_telegram_event(update_json):
     update = telebot.types.Update.de_json(update_json)

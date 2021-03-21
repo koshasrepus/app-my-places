e57e6944426acb46088510be51b8eed513eb4c8a
Александр Евсеев <alexander.evseev@balance-pl.ru>
2021-03-22 00:59:57 +0300

    updaated options for field locations Paces model

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
 class Palaces(models.Model):
     """Места"""
     title = models.CharField(max_length=100, default=None)
-    place_lat = models.FloatField()
-    place_lon = models.FloatField()
+    place_lat = models.FloatField(null=True)
+    place_lon = models.FloatField(null=True)
     image = models.ImageField()
     user = models.ForeignKey('User', on_delete=models.CASCADE)

468eac09cfc017e0164fadaf9f941e998d769f81
Александр Евсеев <alexander.evseev@balance-pl.ru>
2021-03-23 00:36:14 +0300

    added get set TELEGRAM_TOKEN from environment variables

--- app_my_places/settings.py
+++ app_my_places/settings.py
@@ -133,7 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #-------> TELEGRAM
-TELEGRAM_TOKEN = '1505162116:AAFMJ7ED4hkTkR1kY42lNRhUUvc2MpBZhYY'
+#TELEGRAM_TOKEN = '1505162116:AAFMJ7ED4hkTkR1kY42lNRhUUvc2MpBZhYY'
+
+TELEGRAM_TOKEN = bool(os.getenv('TELEGRAM_TOKEN'))
 
 django_heroku.settings(locals())
 
